04c34136f2eac7a787662052b384c302255da67a
hamden-smaoui <116950925+hamden-smaoui@users.noreply.github.com>
2025-10-09 14:18:48 +0100

    configdatabase5

--- syncDatabase.js
+++ syncDatabase.js
@@ -12,9 +12,9 @@ const syncDatabase = async () => {
     await sequelize.sync({ alter: true, force: false });
     console.log('Base de données synchronisée avec succès');
 
-    // Ferme la connexion
-    await sequelize.close();
-    console.log('Connexion à la base de données fermée');
+    // NE PAS FERMER LA CONNEXION ICI !
+    // await sequelize.close(); // ⛔ SUPPRIMER OU COMMENTER
+
   } catch (err) {
     console.error('Erreur lors de la synchronisation de la base de données :', err);
     process.exit(1);
