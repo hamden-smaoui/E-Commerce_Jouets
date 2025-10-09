3e695910e70892b42d45e19dfd0ac581b31024a8
hamden-smaoui <116950925+hamden-smaoui@users.noreply.github.com>
2025-10-09 13:01:40 +0100

    useroote7

--- next.config.ts
+++ next.config.ts
@@ -9,7 +9,15 @@ images: {
         port: '3001',
         pathname: '**',  // Allows any path under this hostname
       },
+    {
+        protocol: 'https',
+        hostname: 'e-commercejouets-production.up.railway.app',
+      },
     ],
-  },};
+  },
+  env: {
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_BASE_URL,
+  },
+};
 
 export default nextConfig;
