f5cf249f55b1e196f147c4113d7f20f986fbb32f
hamden-smaoui <116950925+hamden-smaoui@users.noreply.github.com>
2025-10-12 15:26:15 +0100

    timeout

--- src/services/api.ts
+++ src/services/api.ts
@@ -5,7 +5,7 @@ const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
 
 const api = axios.create({
   baseURL: API_BASE_URL,
-  timeout: 10000,
+  timeout: 30000,
   headers: {
     'Accept': 'application/json',
   },
