3e0a958e4436c29f8099f4d8e839c635bc1549f7
hamden-smaoui <116950925+hamden-smaoui@users.noreply.github.com>
2025-10-27 12:08:18 +0100

    permissioncloudinary

--- next.config.ts
+++ next.config.ts
@@ -1,22 +1,27 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-images: {
+  images: {
     remotePatterns: [
       {
         protocol: 'http',
         hostname: 'localhost',
         port: '3001',
-        pathname: '**',  // Allows any path under this hostname
+        pathname: '**',
       },
-    {
+      {
         protocol: 'https',
         hostname: 'e-commercejouets-production.up.railway.app',
         pathname: '**'
       },
+      // ✅ AJOUTE CLOUDINARY ICI
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        pathname: '/**',
+      },
     ],
   },
-  
 };
 
 export default nextConfig;
